bfbaf16a3de989c07f702d8746f1a6c01ee1ff44
Tommy <tommy@tom.my>
2024-09-12 15:12:06 +0800

    first publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/jlpt-word-game",
   css: { postcss: "./postcss.config.js" },
 });
