8395e31b5aa205e0ea659c23e964869a19456711
Tommy <tommy@tom.my>
2024-09-16 02:09:40 +0800

    Updates

--- assets/FileLoader-IZfKm98M.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as a}from"./index-Cf66GolY.js";function x({file:r,setFiles:t}){return e.useEffect(()=>(t(o=>[...o,r]),()=>{t(o=>o.filter(n=>n!==r))}),[r,t]),a.jsx(a.Fragment,{})}export{x as F};
