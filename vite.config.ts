a54c6a5d1d97cd4d6e5cc3665b7a83c543f1f783
Tommy <tommy@tom.my>
2024-09-21 16:34:49 +0800

    setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/jlpt-word-game",
   css: { postcss: "./postcss.config.js" },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "JLPT 単語",
+        short_name: "JLPT 単語",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
