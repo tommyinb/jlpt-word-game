4f1aaaba0adf4ef7d027a6bfe66963a7c20d7c9e
Tommy <tommy@tom.my>
2024-09-29 21:51:31 +0800

    Updates

--- assets/FileLoader-BNI2UNoF.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as a}from"./index-BH7V8FgJ.js";function x({file:r,setFiles:t}){return e.useEffect(()=>(t(o=>[...o,r]),()=>{t(o=>o.filter(n=>n!==r))}),[r,t]),a.jsx(a.Fragment,{})}export{x as F};
